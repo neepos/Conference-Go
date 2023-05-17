0efab5043d3bec9f30aaf2ec593f01c90633f4b9
mac stephens <macstephens@gmail.com>
2023-05-17 14:00:43 -0400

    finished yml

--- presentation_workflow/presentation_mailer/consumer.py
+++ presentation_workflow/presentation_mailer/consumer.py
@@ -1,13 +1,16 @@
 import json
 import pika
+from pika.exceptions import AMQPConnectionError
 import django
 import os
 import sys
+import time
 from django.core.mail import send_mail
 
 sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "presentation_mailer.settings")
 django.setup()
+
 
 def process_approval(ch, method, properties, body):
     print(" Received %r" % body)
@@ -42,23 +45,28 @@ def process_rejection(ch, method, properties, body):
         [email],
     )
 
-parameters = pika.ConnectionParameters(host="rabbitmq")
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
+while True:
+    try:
+        parameters = pika.ConnectionParameters(host="rabbitmq")
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
 
-channel.queue_declare(queue="presentation_approvals")
-channel.basic_consume(
-    queue="presentation_approvals",
-    on_message_callback=process_approval,
-    auto_ack=True,
-)
+        channel.queue_declare(queue="presentation_approvals")
+        channel.basic_consume(
+            queue="presentation_approvals",
+            on_message_callback=process_approval,
+            auto_ack=True,
+        )
 
-channel.queue_declare(queue="presentation_rejections")
-channel.basic_consume(
-    queue="presentation_rejections",
-    on_message_callback=process_rejection,
-    auto_ack=True,
-)
+        channel.queue_declare(queue="presentation_rejections")
+        channel.basic_consume(
+            queue="presentation_rejections",
+            on_message_callback=process_rejection,
+            auto_ack=True,
+        )
 
-channel.start_consuming()
+        channel.start_consuming()
+    except AMQPConnectionError:
+        print("Could not connect to RabbitMQ")
+        time.sleep(2.0)
 
